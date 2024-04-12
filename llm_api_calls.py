f1c7252103e82f847818c5c8810980c3de7f6006
nikolay tolstov <datatraxer@gmail.com>
2024-04-12 23:18:21 +0700

    added step2 of anames. also ask for choosed ferments

--- llm_api_calls.py
+++ llm_api_calls.py
@@ -3,8 +3,13 @@ import google.generativeai as genai
 import asyncio
 from private import GEMINI_API_KEY, OPENROUTER_API_KEY
 from datetime import datetime, timedelta
-import requests
+import requests, copy
 import json
+from openai import AsyncOpenAI
+
+import google.generativeai as genai
+from private import GEMINI_API_KEY
+import asyncio
 
 
 
@@ -28,6 +33,60 @@ class RateLimiter:
 
         self.calls.append(datetime.now())
 
+
+
+async def send_message_to_openai_async(history, model='gpt-3.5-turbo', rate_limiter=None, attempt=1, max_attempts=10, retry_delay=1, generation_params={}):
+    client = AsyncOpenAI()
+    
+    if rate_limiter is not None:
+        await rate_limiter.wait()
+
+    # Adjust history to match OpenAI's expected format
+    messages = []
+    for num, h in enumerate(history):
+        if h.get('meta_info'):
+            if h.get('meta_info').get('system') == True:
+                role = 'system'
+
+        content = h.get('content') or (h['parts'][0] if 'parts' in h else " ")
+        role = h['role']
+        if role == 'model': 
+            role = 'assistant'
+        messages.append({"role": role, "content": content})
+
+    # Default generation parameters if not specified
+    default_params = {
+        "model": model,
+        "temperature": 0.1,
+        # "max_tokens": 256,
+        "top_p": 1,
+        "frequency_penalty": 0,
+        "presence_penalty": 0
+    }
+
+    generation_params = {**default_params, **generation_params}
+
+    # try:
+    response = await client.chat.completions.create(
+        **generation_params,
+        messages=messages
+    )
+    
+    text_response = response.choices[0].message.content
+    input_tokens = response.usage.prompt_tokens
+    output_tokens = response.usage.completion_tokens
+
+    # except Exception as e:
+    #     print(f'Exception {e}')
+    #     text_response = 'error in response'
+    #     input_tokens = 0
+    #     output_tokens = 0
+
+    return {
+        "text_response": text_response, 
+        "input_tokens": input_tokens, 
+        "output_tokens": output_tokens
+    }
 
 
 def send_message_to_gemini(user_input):
@@ -61,24 +120,32 @@ def send_message_to_gemini(user_input):
             "output_tokens":gemini.count_tokens(gemini_response_text)}
     
 
-import google.generativeai as genai
-from private import GEMINI_API_KEY
+
 import asyncio
+
+
 
 
 async def send_message_to_gemini_async(history, rate_limiter=None, attempt=1, max_attempts=10, retry_delay = 1, generation_params={}):
     if rate_limiter is not None: await rate_limiter.wait()
 
     genai.configure(api_key=GEMINI_API_KEY)
-        
-    history_gemini = history.copy()
-    for h in history_gemini:
-        if h['role'] == 'assistant':
-            h['role'] = 'model'
-        if isinstance(h.get('content'), str):
-            h['parts'] = [h['content']]
-            del h['content']
+            
+    history_gemini = copy.deepcopy(history)
 
+    for item in history_gemini:
+        # Update the role to 'model' if it's originally 'assistant'
+        if item['role'] == 'assistant':
+            item['role'] = 'model'
+        
+        # If 'content' exists and is a string, move it to a list under 'parts' and remove 'content'
+        if isinstance(item.get('content'), str):
+            item['parts'] = [item.pop('content')]
+        
+        # Keep only 'role' and 'parts' keys, removing all others
+        keys_to_remove = [key for key in item if key not in ['role', 'parts']]
+        for key in keys_to_remove:
+            del item[key]
 
     user_input = history_gemini[-1]['parts'][0]
     history_gemini = history_gemini[:-1]
@@ -112,7 +179,7 @@ async def send_message_to_gemini_async(history, rate_limiter=None, attempt=1, ma
         gemini_response_text = convo.last.text
     except Exception as e:
         print(f'Exception {e}')
-        gemini_response_text = 'error in response'
+        gemini_response_text = ' '
 
 
     return {"text_response": gemini_response_text, 
@@ -122,8 +189,17 @@ async def send_message_to_gemini_async(history, rate_limiter=None, attempt=1, ma
 
 
 
-async def send_message_open_router(history, rate_limiter=None, model="anthropic/claude-3-sonnet"):
+async def send_message_open_router_async(history, rate_limiter=None, model="anthropic/claude-3-sonnet"):
     if rate_limiter is not None: await rate_limiter.wait()
+
+    openrouter_message_history = copy.deepcopy(history)
+    for x in openrouter_message_history:
+        if x['role'] == 'model':
+            x['role'] = 'assistant'
+        if isinstance(x.get('parts'), list):
+            x['content'] = x['parts'][0]
+            del x['parts']
+
 
     try:
         response = requests.post(
@@ -137,7 +213,7 @@ async def send_message_open_router(history, rate_limiter=None, model="anthropic/
                 # "model": "anthropic/claude-3-opus",
                 "model": "anthropic/claude-3-sonnet",
                 # "model": "anthropic/claude-3-haiku", # Optional
-                "messages": history
+                "messages": openrouter_message_history
             })
             )
         response_text = response.json()['choices'][0]['message']['content']
